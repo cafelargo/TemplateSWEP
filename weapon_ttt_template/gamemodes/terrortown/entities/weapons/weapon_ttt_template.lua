398d9a314d9f41092a5c6152d1ed15d50045cc5b
cafelargo <clavebreve@outlook.com>
2020-06-28 16:31:13 +0100

    editing code and comments

--- weapon_ttt_template/gamemodes/terrortown/entities/weapons/weapon_ttt_template.lua
+++ weapon_ttt_template/gamemodes/terrortown/entities/weapons/weapon_ttt_template.lua
@@ -7,8 +7,7 @@ if SERVER then
    AddCSLuaFile()
 
    -- Shares the buy menu icon with the client
-
-   resource.AddFile("materials/VGUI/ttt/icon_myserver_ak47.vmt")
+   resource.AddFile("materials/vgui/ttt/blue_template_icon.vmt")
 end
 
 if CLIENT then
@@ -20,7 +19,7 @@ if CLIENT then
    SWEP.DrawCrossHair      = false -- Does the client draw its own crosshair over the default ones
 
    -- Client menu data
-   SWEP.Icon = "<icon missing>" -- The filename of the icon shown in the buymenu
+   SWEP.Icon = "vgui/ttt/blue_template_icon.vtf" -- The filename of the icon shown in the buymenu
    -- The information shown in the buy menu
    SWEP.EquipMenuData      =
    {
@@ -32,31 +31,24 @@ end
 -- What weapon this is based upon (always use weapon_tttbase when creating a ttt/2 weapon)
 SWEP.Base                  = "weapon_tttbase"
 
-
-
 -- Primary fire vals
--- Note, you can also script secondary fire attributes which are for alt fire (SWEP.Secondary)
 SWEP.Primary.Delay         = 0.001 -- Higher is slower
-SWEP.Primary.Automatic     = true
-SWEP.Primary.Damage        = 1
+SWEP.Primary.Automatic     = true -- Is it automatic
+SWEP.Primary.Damage        = 1 -- How much damage per shot
 SWEP.Primary.Cone          = 0.5 -- Cone of accuracy
-SWEP.Primary.Ammo          = "smg1"
+SWEP.Primary.Ammo          = "smg1" -- Which ttt ammo entity is picked up by this swep
 SWEP.Primary.ClipSize      = 100 -- Maximum clip size
 SWEP.Primary.DefaultClip   = 100 -- Starting clip size
-SWEP.Primary.Sound         = Sound("Weapon_AK47.Single")
-SWEP.Primary.NumShots      = 1 -- Number of shots fired at one time
-SWEP.Primary.Recoil        = 1.5 -- Base recoil value
+SWEP.Primary.Sound         = Sound("Weapon_AK47.Single") -- Either a sound file from the default list or a custom one
+SWEP.Primary.NumShots      = 1 -- Number of shots fired at one time (make into shotgun if increase conde val)
+SWEP.Primary.Recoil        = 1.5 -- Recoil value (bigger = bigger)
 
--- View model info
+-- Misc vals
 SWEP.HoldType              = "ar2"
 SWEP.ViewModel             = "models/weapons/c_irifle.mdl" -- What the swep looks like to the user
 SWEP.WorldModel            = "models/weapons/w_irifle.mdl" -- What the swep looks like to everyone else
 SWEP.UseHands              = true -- Whether the swep will force the user to see the viewmodel's hands
- SWEP.Kind                 = WEAPON_EQUIP1 -- Kind specifies what weapon_ttt category it falls into
---[[
-   (        0      1      2       3      4     5      6       7       8   ) +1 to get the eqip slot key
-   (WEAPON_ MELEE, HEAVY, PISTOL, HEAVY, NADE, CARRY, EQUIP1, EQUIP2, ROLE)
---]]
+SWEP.Kind                 = WEAPON_EQUIP1 -- Kind specifies what weapon_ttt category it falls into
 SWEP.AutoSpawnable         = false -- Whether the swep will spawn upon map gen (override as false if EQUIP1/2)
 SWEP.AmmoEnt               = "item_ammo_smg1_ttt" -- Which entity you pick up to fill your clips
 SWEP.CanBuy                = {ROLE_TRAITOR} -- Which roles can purchase this swep (table)
@@ -65,7 +57,7 @@ SWEP.LimitedStock          = true -- If true only one can be bought per round
 SWEP.AllowDrop             = true -- Is the player able to drop the swep
 SWEP.IsSilent              = false -- If true, the killed player will not scream upon death
 SWEP.NoSights              = false -- If true, the swep has no ironsights capability (no Secondary fire if ironsights?)
-SWEP.HeadshotMultiplier    = 50
+SWEP.HeadshotMultiplier    = 50 -- Multiply the headshot damage by this much
 SWEP.DeploySpeed           = 0.2 -- Lower the slower
 SWEP.IronSightsPos         = Vector(-2, -5, 0) -- Moves the Viewmodel by this vector when using ironsights
 SWEP.IronSightsAng         = Vector(-2, 0, 0) -- Rotates the Viewmodel by this angle when using ironsights
@@ -73,6 +65,6 @@ SWEP.IronSightsAng         = Vector(-2, 0, 0) -- Rotates the Viewmodel by this a
 
 -- REDUNDANT SETTINGS (read them because they may still be appicable) --
 
--- DO NOT set SWEP.WeaponID. Only the standard TTT weapons can have it. Custom
+-- Don't set SWEP.WeaponID. Only the standard TTT weapons can have it. Custom
 -- SWEPs do not need it for anything.
 --	SWEP.WeaponID = nil
